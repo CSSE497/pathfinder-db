6777b03ccc9014cfd8016e443d2ed2c432ce5b71
danhanson <danielghanson93@gmail.com>
2015-12-02 02:48:25 -0500

    added subcluster support

--- 1.sql
+++ 1.sql
@@ -1,6 +1,7 @@
 # --- !Ups
 create table cluster (
   id                        bigserial not null,
+  parent_id                 bigint,
   authentication_token      bytea not null,
   constraint pk_cluster primary key (id))
 ;
@@ -14,6 +15,7 @@ create table commodity (
   status                    integer not null,
   param                     integer,
   cluster_id                bigint,
+  constraint ck_commodity_status check (status in ('3','0','1','4','2')),
   constraint pk_commodity primary key (id))
 ;
 
@@ -39,17 +41,22 @@ create table vehicle (
   capacity                  integer not null,
   status                    integer not null,
   cluster_id                bigint,
+  constraint ck_vehicle_status check (status in ('0','1')),
   constraint pk_vehicle primary key (id))
 ;
 
-alter table commodity add constraint fk_commodity_cluster_1 foreign key (cluster_id) references cluster (id);
-create index ix_commodity_cluster_1 on commodity (cluster_id);
-alter table pathfinder_application add constraint fk_pathfinder_application_clus_2 foreign key (cluster_id) references cluster (id);
-create index ix_pathfinder_application_clus_2 on pathfinder_application (cluster_id);
-alter table vehicle add constraint fk_vehicle_cluster_3 foreign key (cluster_id) references cluster (id);
-create index ix_vehicle_cluster_3 on vehicle (cluster_id);
+alter table cluster add constraint fk_cluster_parentCluster_1 foreign key (parent_id) references cluster (id) on delete restrict on update restrict;
+create index ix_cluster_parentCluster_1 on cluster (parent_id);
+alter table commodity add constraint fk_commodity_cluster_2 foreign key (cluster_id) references cluster (id) on delete restrict on update restrict;
+create index ix_commodity_cluster_2 on commodity (cluster_id);
+alter table pathfinder_application add constraint fk_pathfinder_application_clus_3 foreign key (cluster_id) references cluster (id) on delete restrict on update restrict;
+create index ix_pathfinder_application_clus_3 on pathfinder_application (cluster_id);
+alter table vehicle add constraint fk_vehicle_cluster_4 foreign key (cluster_id) references cluster (id) on delete restrict on update restrict;
+create index ix_vehicle_cluster_4 on vehicle (cluster_id);
+
 
 # --- !Downs
+SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists cluster cascade;
 
@@ -60,3 +67,5 @@ drop table if exists pathfinder_application cascade;
 drop table if exists pathfinder_user cascade;
 
 drop table if exists vehicle cascade;
+
+SET REFERENTIAL_INTEGRITY TRUE;
