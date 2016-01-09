1e4190cf6376ce98c8f4cf2e441d6ee1cf51d366
Adam Michael <adam@ajmichael.net>
2016-01-09 18:38:18 -0500

    Updated schema for application changes.

--- 1.sql
+++ 1.sql
@@ -60,35 +60,7 @@ ALTER TABLE commodity_id_seq OWNER TO pathfinderwebserver;
 ALTER SEQUENCE commodity_id_seq OWNED BY commodity.id;
 
 
-CREATE TABLE pathfinder_application (
-    id uuid NOT NULL,
-    name character varying(255),
-    token bytea,
-    cluster_id bigint
-);
-
-
-ALTER TABLE pathfinder_application OWNER TO pathfinderwebserver;
-
-CREATE TABLE pathfinder_user (
-    username character varying(255) NOT NULL,
-    password character varrying(255) NOT NULL,
-    user_token bytea NOT NULL
-);
-
-
-ALTER TABLE pathfinder_user OWNER TO pathfinderwebserver;
-
-CREATE SEQUENCE pathfinder_user_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
 ALTER TABLE commodity_id_seq OWNER TO pathfinderwebserver;
-
-ALTER SEQUENCE commodity_id_seq OWNED BY pathfinder_user.id;
 
 
 CREATE TABLE vehicle (
@@ -123,17 +95,11 @@ ALTER TABLE ONLY cluster ALTER COLUMN id SET DEFAULT nextval('cluster_id_seq'::r
 ALTER TABLE ONLY commodity ALTER COLUMN id SET DEFAULT nextval('commodity_id_seq'::regclass);
 
 
-ALTER TABLE ONLY pathfinder_user ALTER COLUMN id set DEFAULT nextval('pathfinder_user_id_seq'::regclass);
-
-
 ALTER TABLE ONLY vehicle ALTER COLUMN id SET DEFAULT nextval('vehicle_id_seq'::regclass);
-
 
 SELECT pg_catalog.setval('cluster_id_seq', 1, false);
 
 SELECT pg_catalog.setval('commodity_id_seq', 1, false);
-
-SELECT pg_catalog.setval('pathfinder_user_id_seq', 1, false);
 
 SELECT pg_catalog.setval('vehicle_id_seq', 1, false);
 
@@ -146,12 +112,6 @@ ALTER TABLE ONLY commodity
     ADD CONSTRAINT pk_commodity PRIMARY KEY (id);
 
 
-ALTER TABLE ONLY pathfinder_application
-    ADD CONSTRAINT pk_pathfinder_application PRIMARY KEY (id);
-
-ALTER TABLE ONLY pathfinder_user
-    ADD CONSTRAINT pk_pathfinder_user PRIMARY KEY (id);
-
 ALTER TABLE ONLY vehicle
     ADD CONSTRAINT pk_vehicle PRIMARY KEY (id);
 
@@ -162,11 +122,7 @@ CREATE INDEX ix_cluster_parent_id ON cluster USING btree (parent_id);
 CREATE INDEX ix_commodity_cluster_id ON commodity USING btree (cluster_id);
 
 
-CREATE INDEX ix_pathfinder_application_cluster_id ON pathfinder_application USING btree (cluster_id);
-
-
 CREATE INDEX ix_vehicle_cluster_id ON vehicle USING btree (cluster_id);
-
 
 
 ALTER TABLE ONLY cluster
@@ -177,12 +133,54 @@ ALTER TABLE ONLY commodity
     ADD CONSTRAINT fk_commodity_cluster_id FOREIGN KEY (cluster_id) REFERENCES cluster(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 
-ALTER TABLE ONLY pathfinder_application
-    ADD CONSTRAINT fk_pathfinder_application_cluster_id FOREIGN KEY (cluster_id) REFERENCES cluster(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
-
-
 ALTER TABLE ONLY vehicle
     ADD CONSTRAINT fk_vehicle_cluster_id FOREIGN KEY (cluster_id) REFERENCES cluster(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+
+create table application (
+  id                        varchar(255) not null,
+  name                      varchar(255) not null,
+  customer_email            varchar(255),
+  cluster_id                bigint not null,
+  objective_function_id     varchar(255) not null,
+  constraint pk_application primary key (id))
+;
+
+create table capacity_parameter (
+  id                        bigserial not null,
+  application_id            varchar(255) not null,
+  parameter                 varchar(255) not null,
+  constraint pk_capacity_parameter primary key (id))
+;
+
+create table customer (
+  email                     varchar(255) not null,
+  password                  varchar(255) not null,
+  constraint pk_customer primary key (email))
+;
+
+create table objective_function (
+  id                        varchar(255) not null,
+  function                  varchar(255),
+  constraint pk_objective_function primary key (id))
+;
+
+create table objective_parameter (
+  id                        bigserial not null,
+  application_id            varchar(255) not null,
+  parameter                 varchar(255) not null,
+  constraint pk_objective_parameter primary key (id))
+;
+
+alter table application add constraint fk_application_customer_1 foreign key (customer_email) references customer (email);
+create index ix_application_customer_1 on application (customer_email);
+alter table application add constraint fk_application_objectiveFuncti_2 foreign key (objective_function_id) references objective_function (id);
+create index ix_application_objectiveFuncti_2 on application (objective_function_id);
+alter table capacity_parameter add constraint fk_capacity_parameter_applicat_3 foreign key (application_id) references application (id);
+create index ix_capacity_parameter_applicat_3 on capacity_parameter (application_id);
+alter table objective_parameter add constraint fk_objective_parameter_applica_4 foreign key (application_id) references application (id);
+create index ix_objective_parameter_applica_4 on objective_parameter (application_id);
+
 
 
 # --- !Downs
@@ -193,9 +191,6 @@ drop index if exists ix_cluster_parent_id;
 alter table commodity drop constraint if exists fk_commodity_cluster_id;
 drop index if exists ix_commodity_cluster_id;
 
-alter table pathfinder_application drop constraint if exists fk_pathfinder_application_cluster_id;
-drop index if exists ix_pathfinder_application_cluster_id;
-
 alter table vehicle drop constraint if exists fk_vehicle_cluster_id;
 drop index if exists ix_vehicle_cluster_id;
 
@@ -203,9 +198,14 @@ drop table if exists cluster;
 
 drop table if exists commodity;
 
-drop table if exists pathfinder_application;
-
-drop table if exists pathfinder_user;
-
 drop table if exists vehicle;
 
+drop table if exists application cascade;
+
+drop table if exists capacity_parameter cascade;
+
+drop table if exists customer cascade;
+
+drop table if exists objective_function cascade;
+
+drop table if exists objective_parameter cascade;
